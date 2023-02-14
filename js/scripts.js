2fd7a41a8248783b7cfebc7fde0beb0e075a32ed
Eduard Kupin <webmaestro@gmail.com>
2023-02-14 11:47:42 +0200

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+
+// Burger
+
+let burger = document.querySelector("#burger-icon");
+let header = document.querySelector("#header");
+burger.onclick = function() {
+    header.classList.toggle("menu-open");
+}
